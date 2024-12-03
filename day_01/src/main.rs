c54c7a113d07d664bdf380bf07baaafe96943925
notalfredo <icealfredo123@gmail.com>
2024-12-02 19:04:52 -0600

    day one part two

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,10 +1,16 @@
-use std::{cmp::Ordering, collections::BinaryHeap, usize};
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap},
+    usize,
+};
 
 fn main() {
     let file = include_str!("../input/input.txt");
+    //let file = include_str!("../input/example.txt");
 
     let mut left_map: BinaryHeap<usize> = BinaryHeap::new();
     let mut right_map: BinaryHeap<usize> = BinaryHeap::new();
+    let mut page_map: HashMap<usize, usize> = HashMap::new();
 
     for row in file.split('\n').into_iter() {
         if row.is_empty() {
@@ -18,20 +24,31 @@ fn main() {
 
         left_map.push(lhs);
         right_map.push(rhs);
+
+        if page_map.contains_key(&rhs) {
+            let key = page_map.get_mut(&rhs).unwrap();
+            *key += 1;
+        } else {
+            page_map.insert(rhs, 1);
+        }
     }
 
-    let mut sum: usize = 0;
+    let mut sum_q1: usize = 0;
+    let mut sum_q2: usize = 0;
 
     while !left_map.is_empty() && !right_map.is_empty() {
         let lhs = left_map.pop().unwrap();
         let rhs = right_map.pop().unwrap();
 
-        println!("{:} | {:}", lhs, rhs);
-
         let distance: usize = (((lhs as isize) - (rhs as isize)).abs()) as usize;
 
-        sum += distance;
+        sum_q1 += distance;
+
+        if let Some(freq) = page_map.get_mut(&lhs) {
+            sum_q2 += lhs * (*freq);
+        }
     }
 
-    println!("q1: {:}", sum);
+    println!("q1: {:}", sum_q1);
+    println!("q2: {:}", sum_q2);
 }
